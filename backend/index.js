b130049d0ca8f2fe4c5c11a8d9b6db0217a58de1
Linhttn1000 <88160213+Linhttn1000@users.noreply.github.com>
2021-11-17 00:42:36 +0700

    Login and Register System

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
 
 dotenv.config();
@@ -11,13 +12,11 @@ app.use(express.json());
 mongoose
 .connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(()=>{
-    console.log("MongoDB Connect Successful!")
-})
+    useUnifiedTopology: true, })
+.then(()=>{ console.log("MongoDB Connect Successful!")})
 .catch((err) => console.log(err));
 
+app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
 app.listen(3000,()=>{
