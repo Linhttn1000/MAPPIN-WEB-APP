fdb92d5e56e55e470dd253ec46d58c186c2f3ebb
Linhttn1000 <88160213+Linhttn1000@users.noreply.github.com>
2021-11-18 03:50:34 +0700

    Creating Mapbox Marker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import {Room} from "@material-ui/icons";
 
 function App() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 46.2830,
-    longitude: 86.6700,
+    latitude: 10.762622,
+    longitude: 106.660172,
     zoom: 4
   });
 
@@ -16,7 +17,18 @@ function App() {
       {...viewport}
       mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      mapStyle = "mapbox://styles/ngoclinhtt/ckw3zuiri03sz14nukderqm1q"
+    >
+
+      <Marker 
+        latitude={10.823099} 
+        longitude={106.629664} 
+        offsetLeft={-20} 
+        offsetTop={-10}
+      >
+        <Room style = {{fontSize: viewport.zoom * 7, color: "orange"}}/> {/* thu phong con Pin theo action zoom */}
+      </Marker>
+      </ReactMapGL>  
     </div>
   );
 }
