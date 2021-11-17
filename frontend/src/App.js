5642f67d445cc7621dc70992ed68145f92a066e1
Linhttn1000 <88160213+Linhttn1000@users.noreply.github.com>
2021-11-18 04:08:50 +0700

    Creating Mapbox Popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
-import {Room} from "@material-ui/icons";
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import {Room, Star} from "@material-ui/icons";
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -28,6 +28,31 @@ function App() {
       >
         <Room style = {{fontSize: viewport.zoom * 7, color: "orange"}}/> {/* thu phong con Pin theo action zoom */}
       </Marker>
+
+      <Popup
+          latitude={10.823099}
+          longitude={106.629664}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div className = "card">
+            <label>Place</label>
+            <h4 className = "place">Ho Chi Minh City</h4>
+            <label>Review</label>
+            <p>Beautiful city. I love it.</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star/>
+              <Star/>
+              <Star/>
+              <Star/>
+              <Star/>
+            </div>
+            <label>Information </label>
+            <span className="username">Created by <b>NgocLinh</b> </span>
+            <span className="date">1 hours ago</span>
+          </div>
+        </Popup>
       </ReactMapGL>  
     </div>
   );
