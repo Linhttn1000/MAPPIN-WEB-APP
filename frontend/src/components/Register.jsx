8e26fab162138ecb8c726dd93fb034eef03b4a09
Linhttn1000 <88160213+Linhttn1000@users.noreply.github.com>
2021-11-22 04:08:13 +0700

    Mern Stack Register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,26 +1,52 @@
 import { Cancel, Room } from "@material-ui/icons";
+import axios from "axios";
+import { useRef, useState } from "react";
 import "./Register.css";
 
 function Register() {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    try {
+      await axios.post("/users/register", newUser);
+      setError(false);
+      setSuccess(true);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="registerContainer">
       <div className="logo">
         <Room />
         TravelPin
       </div>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" ref={nameRef} />
+        <input type="email" placeholder="Email" ref={emailRef} />
+        <input type="password" placeholder="Password" ref={passwordRef} />
         <button className="registerBtn">Register</button>
-        <span className="success">
-          {" "}
-          Successfully registered. You can login now.
-        </span>
-        <span className="failure">
-          Username , Email or Password went wrong.
-        </span>
-        >
+        {success && (
+          <span className="success">
+            Successfully registered. You can login now.
+          </span>
+        )}{" "}
+        {error && (
+          <span className="failure">
+            Username , Email or Password went wrong.
+          </span>
+        )}
       </form>
     </div>
   );
